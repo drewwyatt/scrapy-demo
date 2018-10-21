6a3957e7ca36e05c0c5de1eb0460c590d4b122c0
Drew Wyatt <drew.j.wyatt@gmail.com>
2018-10-21 16:36:51 -0400

    renamed ScrapeItem to PartItem

--- scrape/items.py
+++ scrape/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class ScrapeItem(scrapy.Item):
+class PartItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     partNumber = scrapy.Field()
